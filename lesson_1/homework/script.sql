14d995b38d09f464f1093e6e063844c319c30028
DilyorSodikov <dilyor.sodiqov@mail.ru>
2025-04-15 18:01:49 +0500

    script.sql

--- lesson_1/homework/script.sql
+++ lesson_1/homework/script.sql
@@ -1,1 +1,24 @@
-SELECT
+CREATE DATABASE SchoolDB;
+USE SchoolDB;
+
+CREATE TABLE Students(
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT
+);
+
+INSERT INTO Students(StudentID, Name, Age)
+VALUES (15,'Dilyor', 21);
+
+INSERT INTO Students(StudentID, Name, Age)
+VALUES (16,'Asilbek', 30);
+
+INSERT INTO Students(StudentID, Name, Age)
+VALUES (17,'Kamron', 16);
+
+SELECT * FROM Students
+
+
+
+
+
