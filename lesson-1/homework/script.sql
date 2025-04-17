9c16e1f84d8943cc225fdd58d69d718c80ff2043
DilyorSodikov <dilyor.sodiqov@mail.ru>
2025-04-17 11:26:46 +0500

    Update script.sql

--- lesson-1/homework/script.sql
+++ lesson-1/homework/script.sql
@@ -1,1 +1,22 @@
-SELECT
+CREATE DATABASE SchoolDB;
+USE SchoolDB;
+
+CREATE TABLE Students(
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Age INT
+);
+
+INSERT INTO Students(StudentID, Name, Age)
+VALUES (15,'Dilyor', 21);
+
+INSERT INTO Students(StudentID, Name, Age)
+VALUES (16,'Asilbek', 30);
+
+INSERT INTO Students(StudentID, Name, Age)
+VALUES (17,'Kamron', 16);
+
+SELECT * FROM Students
+
+-- -- BACKUP DATABASE SchoolDB
+-- -- TO DISK = '/var/opt/mssql/data/SchoolDB.bak';
